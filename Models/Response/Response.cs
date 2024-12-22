2656b4e884bb40741e2d19f5de7694f79ae63805
shielamae02 <shiela.mlepon@gmail.com>
2024-12-23 02:54:17 +0800

    chore: add ApiResponse class for standardized API responses

---
+++ Models/Response/Response.cs
@@ -0,0 +1,23 @@
+
+using Newtonsoft.Json;
+using static ToDoAPI_ASPNET.Models.Utils.Error;
+
+namespace ToDoAPI_ASPNET.Models.Response;
+
+public class ApiResponse<T>
+{
+    public string Status { get; init; } = null!;
+    public string Message { get; init; } = null!;
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public T? Data { get; init; }
+
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public ErrorType? ErrorType { get; init; }
+
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public Dictionary<string, string>? ValidationErrors { get; init; }
+
+}
