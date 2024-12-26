6275957d64048c1539bd4614c8a7deec9fe3268f
shielamae02 <shiela.mlepon@gmail.com>
2024-12-26 22:34:00 +0800

    chore: update Token entity to rename Expiration to ExpiresAt and add Type column

--- Migrations/20241226130654_UpdateTokenEntity.Designer.cs
+++ Migrations/20241226130654_UpdateTokenEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoAPI_ASPNET.Data;
 
@@ -11,9 +12,11 @@ using ToDoAPI_ASPNET.Data;
 namespace ToDoAPI_ASPNET.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241226130654_UpdateTokenEntity")]
+    partial class UpdateTokenEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
