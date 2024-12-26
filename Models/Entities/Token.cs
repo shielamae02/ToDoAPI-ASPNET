44feb649fbe68403a2fc34e34382d5cd113c4424
shielamae02 <shiela.mlepon@gmail.com>
2024-12-26 22:34:07 +0800

    chore: update Token entity to rename Expiration to ExpiresAt and add TokenType

--- Models/Entities/Token.cs
+++ Models/Entities/Token.cs
@@ -6,11 +6,19 @@ namespace ToDoAPI_ASPNET.Models.Entities;
 [Index(nameof(Value), IsUnique = true)]
 public sealed class Token : BaseEntity
 {
+    public enum TokenType
+    {
+        Refresh,
+        Access
+    }
+
     [ForeignKey(nameof(User))]
     public int UserId { get; init; }
+
     public string Value { get; set; } = null!;
     public bool isRevoked { get; set; } = false;
-    public DateTime Expiration { get; init; }
+    public DateTime ExpiresAt { get; init; }
+    public TokenType Type { get; set; } = TokenType.Refresh;
 
     public User User { get; init; } = null!;
 }
