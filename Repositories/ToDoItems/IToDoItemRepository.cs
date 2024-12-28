60da20e29a55cba1cbd4a4d2b714f7bad0227839
shielamae02 <shiela.mlepon@gmail.com>
2024-12-29 00:29:15 +0800

    feat: add IToDoItemRepository interface for managing ToDo items

---
+++ Repositories/ToDoItems/IToDoItemRepository.cs
@@ -0,0 +1,16 @@
+using ToDoAPI_ASPNET.Models.Entities;
+
+namespace ToDoAPI_ASPNET.Repositories.ToDoItems;
+
+public interface IToDoItemRepository
+{
+    Task<ToDoItem> CreateAsync(ToDoItem toDoItem);
+    Task<ToDoItem?> GetByIdAsync(int id);
+    Task<IEnumerable<ToDoItem>> GetAllAsync(int userId);
+    Task<bool> UpdateAsync(ToDoItem toDoItem);
+    Task<bool> UpdateStatusAsync(IEnumerable<int> itemIds, bool newStatus);
+    Task<bool> UpdateRangeAsync(IEnumerable<ToDoItem> toDoItems);
+    Task<bool> DeleteAsync(int id);
+    Task<bool> DeleteRangeAsync(IEnumerable<int> ids);
+}
+
