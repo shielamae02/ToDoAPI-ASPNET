b304d3f7c96c90db17edfc12b15571282cbcc5a4
shielamae02 <shiela.mlepon@gmail.com>
2024-12-29 00:40:20 +0800

    feat: add GetToDoItemByIdAsync method to ToDoItemService for retrieving ToDo item by ID

--- Services/ToDoItems/ToDoItemService.cs
+++ Services/ToDoItems/ToDoItemService.cs
@@ -27,5 +27,22 @@ public class ToDoItemService(
         );
     }
 
+    public async Task<ApiResponse<ToDoItemDto>> GetToDoItemByIdAsync(int userId, int itemId)
+    {
+        var toDoItem = await toDoItemRepository.GetByIdAsync(itemId);
+        if (toDoItem is null || toDoItem.UserId != userId)
+            return ApiResponse<ToDoItemDto>.ErrorResponse(
+               Error.NotFound,
+               Error.ErrorType.NotFound,
+               new Dictionary<string, string> { { "toDoItem", $"Item with id {itemId} does not exist." } }
+           );
+
+        return ApiResponse<ToDoItemDto>.SuccessResponse(
+           Success.RESOURCE_RETRIEVED("ToDoItem"),
+           mapper.Map<ToDoItemDto>(toDoItem)
+       );
+    }
+
+    
 
 }
